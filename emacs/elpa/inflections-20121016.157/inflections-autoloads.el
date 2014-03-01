79fed05dd01c940439ffa3dbc0c90104b0dc1a25
squatched <squatched@users.noreply.github.com>
2014-02-28 20:20:18 -0800

    Adding rinari to my emacs install.

---
+++ emacs/elpa/inflections-20121016.157/inflections-autoloads.el
@@ -0,0 +1,34 @@
+;;; inflections-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (pluralize-string singularize-string) "inflections"
+;;;;;;  "inflections.el" (21265 23979 613751 470000))
+;;; Generated autoloads from inflections.el
+
+(autoload 'singularize-string "inflections" "\
+
+
+\(fn STR)" nil nil)
+
+(autoload 'pluralize-string "inflections" "\
+
+
+\(fn STR)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("inflections-pkg.el") (21265 23979 718111
+;;;;;;  127000))
+
+;;;***
+
+(provide 'inflections-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; inflections-autoloads.el ends here
