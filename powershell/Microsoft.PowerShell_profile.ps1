14cfdac49d960bd1bdf3187e36b6ffdf8646082b
squatched <squatched@users.noreply.github.com>
2017-04-19 09:44:01 -0700

    [Powershell] Make module initialization optional and add support for initing posh-git

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -9,9 +9,24 @@
 
 [String]$profilePath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
+function Internal_Helper_Import_Module ([String]$moduleName) {
+        if ( !( Get-Module -ListAvailable -Name $moduleName ) ) {
+                Write-Debug "Module <$moduleName> is not installed. Skipping import."
+                return $false
+        }
+
+        Import-Module PSReadLine
+        return $true
+}
+
 ## Import PowerTab
 #Import-Module PowerTab -Argumentlist ($profilePath + "\PowerTabConfig.xml")
 
 ## Import PSReadline - See https://github.com/lzybkr/PSReadLine
-Import-Module PSReadLine
-Set-PSReadlineOption -EditMode Emacs
+if ( Internal_Helper_Import_Module( "PSReadLine" ) ) {
+        Set-PSReadlineOption -EditMode Emacs
+}
+
+## Import posh-git - See https://github.com/dahlbyk/posh-git
+if ( Internal_Helper_Import_Module( "posh-git" ) ) {
+}
