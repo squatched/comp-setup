ebd620ad5acd8ed110fc7cbef25c1bc052b3956e
squatched <squatched@users.noreply.github.com>
2022-09-19 18:31:30 -0700

    [emacs] Remove "emacs-vanilla", Fix Build Error
    Finally getting rid of emacs-vanilla and moving doom stuff to a less
    verbose emacs/doom setup.
    
    Also removing a reference to a deprecated module (ui-fill-line) and
    then adding use of a new global param to accomplish the same thing w/o an
    entire module.

--- emacs-vanilla/elpa/batch-mode-20140807.2050/batch-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "batch-mode" "20140807.2050" "major mode for editing ESRI batch scrips" 'nil)
--- emacs-vanilla/elpa/findr-20130824.1207/findr-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; findr-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("findr-pkg.el" "findr.el") (21265 23980
-;;;;;;  221691 565000))
-
-;;;***
-
-(provide 'findr-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; findr-autoloads.el ends here
